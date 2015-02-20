b5e0c0dc5c66bca21fd339530a337592270f8fd9
Vinay <vinay.krupakaran@gmail.com>
2015-02-20 12:26:41 +0000

    Problem 4
    Added solution to problem 4

---
+++ Prob_4_palin_product.py
@@ -0,0 +1,30 @@
+#This program computes the largest palindrome made from the product 
+#of two 3-digit numbers
+
+def check_palin(num):
+#This function checks if a number is a palindrome
+	num_str = str(num) #convert the number to a string
+	num_str = num_str[::-1] #reverse the string
+	if num == int(num_str):
+		return True
+
+def main():
+#Main function
+	largest = limit = 0
+	n1 = 0
+	i = j = 999
+	for i in xrange(999, 100, -1):
+		for j in xrange(999, 100, -1):
+			prod = i * j
+			check = check_palin(prod)
+			if check == True:
+				if prod > largest:
+					largest = prod
+					limit = j
+					n1 = i
+					break
+		if i < limit:
+			break
+	print '%d is the largest palindrome product with factors %d and %d' %(largest, n1, limit)
+
+main()
